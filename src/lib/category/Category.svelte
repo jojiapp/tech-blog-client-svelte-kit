1a07d48c58f0b8244898d75575e556cd94f83936
joji <jojiapp@gmail.com>
2022-08-23 08:22:40 +0900

    [Refac: Category] class flag 방식 변경

--- src/lib/category/Category.svelte
+++ src/lib/category/Category.svelte
@@ -4,28 +4,27 @@
 
 	let selectCategory = categories[0];
 
-	let isHidden = true;
+	let isActive = false;
 
-	const handleIsHiddenToggle = () => (isHidden = !isHidden);
+	const handleIsActiveToggle = () => (isActive = !isActive);
 
 	const handleCategoryClick = (category) => {
 		selectCategory = category;
-		handleIsHiddenToggle();
+		handleIsActiveToggle();
 	};
 </script>
 
 <div class='relative z-10 w-48'>
 	<div class='border-2 border-neutral-800 p-2'
-			 on:click={handleIsHiddenToggle}>
+			 on:click={handleIsActiveToggle}>
 		<div class='flex cursor-pointer items-center'>
 			{selectCategory}
 		</div>
 	</div>
-	<div class="{isHidden ? 'hidden' : ''}
-		absolute top-14 w-48 border-2 border-neutral-800 bg-neutral-900 py-3 px-2">
+	<div class='absolute top-14 w-48 border-2 border-neutral-800 bg-neutral-900 py-3 px-2'
+			 class:hidden={!isActive}>
 		{#each categories as category, index}
-			<div class="{index === CATEGORIES_LAST_INDEX ? '' : 'pb-4'}
-			cursor-pointer"
+			<div class='cursor-pointer p-3'
 					 on:click={() => handleCategoryClick(category)}>
 				{category}
 			</div>
