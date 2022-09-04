2316f750b9f244a77fd25dcba63d06bb60655ba8
joji <jojiapp@gmail.com>
2022-09-05 06:50:50 +0900

    [Feat: gh-pages]

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = 'production' === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -15,6 +17,10 @@ const config = {
 			assets: 'docs',
 			fallback: '200.html'
 		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? '' : '/tech-blog-client-svelte-kit'
+		},
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
